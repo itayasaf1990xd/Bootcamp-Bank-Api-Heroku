d892e54971919932a7bf42c0654a6b40d1c19b7a
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2022-01-23 02:19:47 +0200

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -5,20 +5,16 @@ const path = require("path");
 const rootRouter = require("./routes/root.router");
 const apiRouter = require("./routes/api.router")
 const publicPath = path.join(__dirname, 'build');
-
 const port = process.env.PORT || 8080;
-
 const app = express();
+
 app.use(cors());
 app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/", rootRouter);
 app.use("/api/users", apiRouter);
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
-
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
