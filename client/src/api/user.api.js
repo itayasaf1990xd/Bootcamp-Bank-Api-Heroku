d3ea68f5794e0e7f002f19e46d76b0eb5f645905
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2022-02-02 22:27:59 +0200

    Add files via upload

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
-// Development
+/* Development (LocalHost)
+ * Client: http://localhost:3000/
+ * Server: http://localhost:8080/api/users/endpoint
+*/
 let url = 'http://localhost:8080/api/';
 
+/* Production (Heroku)
+ * Client: https://full-stack-template.herokuapp.com/
+ * Server: https://full-stack-template.herokuapp.com/api/users/endpoint
+*/
 if (process.env.NODE_ENV === 'production') {
   url = 'api';
 }
